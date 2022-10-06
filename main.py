01fd7578c54e788d1a57d57dc675f25d550e0e6d
lemter <karim0003b@gmail.com>
2022-10-06 13:59:33 +0600

    new pages, general style file

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html")
+
+@app.route("/books")
+def books():
+    return render_template("books.html")
+
+@app.route("/readers")
+def readers():
+    return render_template("readers.html")
+
+@app.route("/borrows")
+def borrows():
+    return render_template("borrows.html")
