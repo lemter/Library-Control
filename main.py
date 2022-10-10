a96d9eb9bc2b3b9a706315ea75cb5e3f1a1bcd8c
lemter <karim0003b@gmail.com>
2022-10-10 15:41:59 +0600

    readers back/frontend functional

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 import jyserver.Flask as jsf
 import psycopg2
@@ -15,7 +16,7 @@ cursor = connection.cursor()
 app = Flask(__name__)
 
 @jsf.use(app)
-class Books:
+class LibControl:
     def __init__(self):
         pass
 
@@ -42,6 +43,29 @@ class Books:
         connection.commit()
         self.js.window.location.replace("/books")
 
+    def addReader(self, f_name, l_name):
+        if f_name and l_name:
+            cursor.execute(f"INSERT INTO readers (reader_f_name, reader_l_name) VALUES ('{f_name}', '{l_name}')")
+            connection.commit()
+            self.js.window.location.reload()
+        else:
+            self.js.alert("All text fields must be filled.")
+
+    def changeReaderName(self, reader_id, new_name):
+        cursor.execute(f"UPDATE readers SET reader_f_name = '{new_name}' WHERE reader_id = {reader_id}")
+        connection.commit()
+        self.js.window.location.reload()
+
+    def changeReaderLastName(self, reader_id, new_lastname):
+        cursor.execute(f"UPDATE readers SET reader_l_name = '{new_lastname}' WHERE reader_id = {reader_id}")
+        connection.commit()
+        self.js.window.location.reload()
+
+    def deleteReader(self, reader_id):
+        cursor.execute(f"DELETE FROM readers WHERE reader_id = {reader_id}")
+        connection.commit()
+        self.js.window.location.replace("/readers")
+
 @app.route("/")
 @app.route("/home")
 def index():
@@ -52,20 +76,33 @@ def books():
     if request.method == 'GET':
         cursor.execute('SELECT * FROM books ORDER BY book_id ASC')
         books = cursor.fetchall()
-        return Books.render(render_template("books/books.html", books = books))
+        return LibControl.render(render_template("books/books.html", books = books))
 
 @app.route("/books/<book_id>", methods=['POST', 'GET'])
-def bookSetting(book_id):
+def bookSettings(book_id):
     if request.method == 'GET':
         cursor.execute(f"SELECT book_name, book_author FROM books WHERE book_id = {book_id}")
         book = cursor.fetchone()
         book_name = book[0]
         book_author = book[1]
-        return Books.render(render_template("books/book_settings.html", book_id = book_id, book_name = book_name, book_author = book_author))
+        return LibControl.render(render_template("books/book_settings.html", book_id = book_id, book_name = book_name, book_author = book_author))
 
-@app.route("/readers")
+@app.route("/readers", methods=['POST', 'GET'])
 def readers():
-    return render_template("readers.html")
+    if request.method == 'GET':
+        cursor.execute('SELECT * FROM readers ORDER BY reader_f_name, reader_l_name ASC')
+        readers = cursor.fetchall()
+        return LibControl.render(render_template("readers/readers.html", readers = readers))
+
+@app.route("/readers/<reader_id>", methods=['POST', 'GET'])
+def readerSettings(reader_id):
+    if request.method == 'GET':
+        cursor.execute(f"SELECT reader_f_name, reader_l_name FROM readers WHERE reader_id = {reader_id}")
+        reader = cursor.fetchone()
+        reader_name = reader[0]
+        reader_lastname = reader[1]
+        return LibControl.render(render_template("readers/reader_settings.html", reader_id = reader_id, reader_name = reader_name, reader_lastname = reader_lastname))
+
 
 @app.route("/borrows")
 def borrows():
