abc1d3c2e3b3965a7c8a9d30cdddf5a8d8930d9c
lemter <karim0003b@gmail.com>
2022-11-08 19:15:43 +0600

    authorization system added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import jyserver.Flask as jsf
 import psycopg2
+from datetime import datetime
 
 connection = psycopg2.connect(
     user = "postgres",
@@ -14,6 +14,8 @@ connection = psycopg2.connect(
 cursor = connection.cursor()
 
 app = Flask(__name__)
+
+app.secret_key = "libcontrol_secretkey"
 
 @jsf.use(app)
 class LibControl:
@@ -66,13 +68,120 @@ class LibControl:
         connection.commit()
         self.js.window.location.replace("/readers")
 
+    def addBorrow(self, reader_id, book_id):
+        if reader_id and book_id:
+            cursor.execute(f"INSERT INTO borrows (reader_id, book_id, taking_date, taking_time) VALUES ({reader_id}, {book_id}, '{datetime.now().strftime('%m/%d/%Y')}', '{datetime.now().strftime('%H:%M')}')")
+            connection.commit()
+            self.js.window.location.reload()
+        else:
+            self.js.alert("All text fields must be filled.")
+
+    def new_b_reader(self, borrow_id, reader_id):
+        print(borrow_id, reader_id)
+        if reader_id:
+            cursor.execute(f"UPDATE borrows SET reader_id = {reader_id} WHERE borrow_id = {borrow_id}")
+            connection.commit()
+            self.js.window.location.reload()
+        else:
+            self.js.alert("You need to write new reader id.")
+
+    def new_b_book(self, borrow_id, book_id):
+        if book_id:
+            cursor.execute(f"UPDATE borrows SET book_id = {book_id} WHERE borrow_id = {borrow_id}")
+            connection.commit()
+            self.js.window.location.reload()
+        else:
+            self.js.alert("You need to write new book id.")
+
+    def new_b_takingtime(self, borrow_id, takingdate, takingtime):
+        if takingdate and takingtime:
+            try:
+                cursor.execute(f"UPDATE borrows SET taking_date = {takingdate}, taking_time = {takingtime} WHERE borrow_id = {borrow_id}")
+                connection.commit()
+            except: self.js.alert("Date or time format writed incorrect.")
+            self.js.window.location.reload()
+        else:
+            self.js.alert("You need to fill taking date and time fields.")
+
+    def submite_borrow(self, borrow_id):
+        cursor.execute(f"UPDATE borrows SET return_date = {datetime.now().strftime('%m/%d/%Y')}, return_time = {datetime.now().strftime('%H:%M')} WHERE borrow_id = {borrow_id}")
+        connection.commit()
+        self.js.window.location.reload()
+
+    def deleteBorrow(self, borrow_id):
+        cursor.execute(f"DELETE FROM borrows WHERE borrow_id = {borrow_id}")
+        connection.commit()
+        self.js.window.location.replace("/borrows")
+
+    def mainpage(self, reader_id):
+        self.js.window.location.replace(f"$reader_id={reader_id}")
+
+    def login(self, login, passw):
+        cursor.execute(f"SELECT * FROM users WHERE username = '{login}' AND password = '{passw}'")
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['username'] = login
+            session['password'] = passw
+            session['level'] = user[2]
+            self.js.window.location.replace('/')
+        else:
+            self.js.alert("Incorrect login or password!")
+
+    def deauth(self):
+        session.pop('loggedin')
+        session.pop('username')
+        session.pop('password')
+        session.pop('level')
+        self.js.window.location.replace('/')
+
+    def regist(self, login, passw, reader_id):
+        cursor.execute(f"INSERT INTO users VALUES('{login}', '{passw}', 0, {reader_id})")
+        connection.commit()
+        self.js.window.location.replace('/')
+
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template("index.html")
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
+    r_id = request.args.get('reader_id')
+    borrows = []
+    if r_id:
+        cursor.execute(f"""
+        SELECT borrow_id,
+               borrows.reader_id,
+               CONCAT(reader_f_name, ' ', reader_l_name) AS "reader_name",
+	           borrows.book_id,
+	           books.book_name,
+	           books.book_author,
+	           borrows.taking_date,
+	           borrows.taking_time,
+	           borrows.return_date,
+	           borrows.return_time,
+	           CASE WHEN (return_date is null AND taking_date + interval '30 day' > CURRENT_DATE) THEN ('Expected')
+	   		        WHEN (return_date is null AND taking_date + interval '30 day' <= CURRENT_DATE) THEN ('Overdue')
+			        WHEN (return_date is not null) THEN ('Submitted')
+			        END AS "status"
+        FROM borrows
+        JOIN readers ON (borrows.reader_id = readers.reader_id)
+        JOIN books ON (borrows.book_id = books.book_id)
+        WHERE borrows.reader_id = {r_id}""")
+        borrows = cursor.fetchall()
+    return LibControl.render(render_template("index.html", borrows = borrows))
 
 @app.route("/books", methods=['POST', 'GET'])
 def books():
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
     if request.method == 'GET':
         cursor.execute('SELECT * FROM books ORDER BY book_id ASC')
         books = cursor.fetchall()
@@ -80,6 +189,12 @@ def books():
 
 @app.route("/books/<book_id>", methods=['POST', 'GET'])
 def bookSettings(book_id):
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
     if request.method == 'GET':
         cursor.execute(f"SELECT book_name, book_author FROM books WHERE book_id = {book_id}")
         book = cursor.fetchone()
@@ -89,6 +204,12 @@ def bookSettings(book_id):
 
 @app.route("/readers", methods=['POST', 'GET'])
 def readers():
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
     if request.method == 'GET':
         cursor.execute('SELECT * FROM readers ORDER BY reader_f_name, reader_l_name ASC')
         readers = cursor.fetchall()
@@ -96,6 +217,12 @@ def readers():
 
 @app.route("/readers/<reader_id>", methods=['POST', 'GET'])
 def readerSettings(reader_id):
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
     if request.method == 'GET':
         cursor.execute(f"SELECT reader_f_name, reader_l_name FROM readers WHERE reader_id = {reader_id}")
         reader = cursor.fetchone()
@@ -104,9 +231,70 @@ def readerSettings(reader_id):
         return LibControl.render(render_template("readers/reader_settings.html", reader_id = reader_id, reader_name = reader_name, reader_lastname = reader_lastname))
 
 
-@app.route("/borrows")
+@app.route("/borrows", methods=['POST', 'GET'])
 def borrows():
-    return render_template("borrows.html")
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
+    if request.method == 'GET':
+        cursor.execute("""
+        SELECT borrow_id,
+               borrows.reader_id,
+               CONCAT(reader_f_name, ' ', reader_l_name) AS "reader_name",
+	           borrows.book_id,
+	           books.book_name,
+	           books.book_author,
+	           borrows.taking_date,
+	           borrows.taking_time,
+	           borrows.return_date,
+	           borrows.return_time,
+	           CASE WHEN (return_date is null AND taking_date + interval '30 day' > CURRENT_DATE) THEN ('Expected')
+	   		        WHEN (return_date is null AND taking_date + interval '30 day' <= CURRENT_DATE) THEN ('Overdue')
+			        WHEN (return_date is not null) THEN ('Submitted')
+			        END AS "status"
+        FROM borrows
+        JOIN readers ON (borrows.reader_id = readers.reader_id)
+        JOIN books ON (borrows.book_id = books.book_id)""")
+        borrows = cursor.fetchall()
+        return LibControl.render(render_template("borrows/borrows.html", borrows = borrows))
+
+@app.route("/borrows/<borrow_id>", methods=['POST', 'GET'])
+def borrow_info(borrow_id):
+    if not 'loggedin' in session: return redirect('/auth')
+    if session["level"] == 0:
+        pass
+    elif session["level"] == 1:
+        pass
+
+    if request.method == 'GET':
+        cursor.execute(f"""
+        SELECT borrow_id,
+               borrows.reader_id,
+               CONCAT(reader_f_name, ' ', reader_l_name) AS "reader_name",
+	           borrows.book_id,
+	           books.book_name,
+	           books.book_author,
+	           borrows.taking_date,
+	           borrows.taking_time,
+	           borrows.return_date,
+	           borrows.return_time,
+	           CASE WHEN (return_date is null AND taking_date + interval '30 day' > CURRENT_DATE) THEN ('Expected')
+	   		        WHEN (return_date is null AND taking_date + interval '30 day' <= CURRENT_DATE) THEN ('Overdue')
+			        WHEN (return_date is not null) THEN ('Submitted')
+			        END AS "status"
+        FROM borrows
+        JOIN readers ON (borrows.reader_id = readers.reader_id)
+        JOIN books ON (borrows.book_id = books.book_id)
+        WHERE borrow_id = {borrow_id}""")
+        borrow = cursor.fetchone()
+        return LibControl.render(render_template("borrows/borrow_info.html", borrow = borrow))
+
+@app.route("/auth")
+def auth():
+    return LibControl.render(render_template('auth.html'))
 
 if __name__ == '__main__':
     app.run()
